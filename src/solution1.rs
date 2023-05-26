ff54a074fc5a98c8c81170b716b3904230f3d698
belyaev-ay <anton.belyaev@sibit.tech>
2023-05-26 11:48:25 +0700

    fix

--- src/solution1.rs
+++ src/solution1.rs
@@ -1,15 +1,26 @@
+use std::collections::HashMap;
+
 struct Solution;
 
+// https://leetcode.com/problems/two-sum
 impl Solution {
     pub fn two_sum(nums: Vec<i32>, target: i32) -> Vec<i32> {
-        for (i, e1) in nums.iter().enumerate() {
-            for (j, e2) in nums.iter().enumerate() {
-                if i != j && e1 + e2 == target {
-                    return vec![i as i32, j as i32]
-                }
+        let mut map: HashMap<&i32, usize> = HashMap::new();
+        for (i, e) in nums.iter().enumerate() {
+            map.insert(e, i);
+        }
+        for (i, e) in nums.iter().enumerate() {
+            let j = map.get(&(target - e));
+            match j {
+                None => continue,
+                Some(it) => {
+                    if *it != i {
+                        return vec![i as i32, *it as i32];
+                    }
+                },
             }
         }
-        return nums
+        return nums;
     }
 }
 
@@ -19,8 +30,9 @@ mod tests {
 
     #[test]
     fn test() {
-        assert_eq!(Solution::two_sum(vec![2,7,11,15], 9), vec![0,1]);
-        assert_eq!(Solution::two_sum(vec![3,2,4], 6), vec![1,2]);
-        assert_eq!(Solution::two_sum(vec![3,3], 6), vec![0,1]);
+        assert_eq!(Solution::two_sum(vec![2, 5, 5, 11], 10), vec![1, 2]);
+        assert_eq!(Solution::two_sum(vec![2, 7, 11, 15], 9), vec![0, 1]);
+        assert_eq!(Solution::two_sum(vec![3, 2, 4], 6), vec![1, 2]);
+        assert_eq!(Solution::two_sum(vec![3, 3], 6), vec![0, 1]);
     }
 }
