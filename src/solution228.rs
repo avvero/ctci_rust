090942e59e889dfeb34cef65ed5e2245bef392a5
belyaev-ay <anton.belyaev@sibit.tech>
2023-06-23 12:09:14 +0700

    fix

--- src/solution228.rs
+++ src/solution228.rs
@@ -7,7 +7,24 @@ struct Solution;
 // 0,1
 impl Solution {
     pub fn summary_ranges(nums: Vec<i32>) -> Vec<String> {
-        return vec![]
+        let mut result = vec![];
+        if nums.len() == 0 {
+            return result
+        }
+        let mut s = nums[0];
+        for (i, _) in nums.iter().enumerate() {
+            if i == nums.len() - 1 || nums[i + 1] as i64 - nums[i] as i64 > 1 {
+                if s == nums[i] {
+                    result.push(format!("{}", s))
+                } else {
+                    result.push(format!("{}->{}", s, nums[i]))
+                }
+                if i < nums.len() - 1 {
+                    s = nums[i + 1]
+                }
+            }
+        }
+        return result
     }
 }
 
